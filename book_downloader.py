c6e1f5a6857fae5ca2af6dbb611680c7ac491b3d
Oxidiovega <tarekvega92@gmail.com>
2022-03-03 15:00:17 +0100

    optimized code and fixed warnings

--- book_downloader.py
+++ book_downloader.py
@@ -1,79 +1,79 @@
 from selenium import webdriver
-import bs4
-import time
-import requests
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.firefox.service import Service
+from selenium.common.exceptions import NoSuchElementException,StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver_path=input('insert the path to your geckodriver.exe')
+# takes a list of books seperated by commas as input
+
 book_names=input('choose a name of your book \n').split(',')
-#takes a list of books seperated by commas as input
-extension=input('choose your extension pdf,epub,txt \n')
-#takes only one extension as a parameter (for now)
 
-#browser=webdriver.Firefox(executable_path='C:\\Users\\Oxidiovega\\Desktop\\geckodriver.exe')
-browser=webdriver.Firefox(executable_path=driver_path)
+# list of supported extension and their css id 
 
+extension_dictionary={'pdf':9,'epub':5,'txt':11}
+
+# takes only one extension as a parameter (for now)
+
+while True:  
+    extension=input('choose your extension pdf,epub,txt \n').strip().lower()
+    if extension not in extension_dictionary.keys():
+        pass
+    else:
+        break
+
+# setting up the browser
+
+s=Service('geckodriver.exe')
+browser = webdriver.Firefox(service=s)
+
+# this opens up the browser and redirects to this website
 browser.get('https://z-lib.org/')
 
-books_button=browser.find_element_by_css_selector('div.col-sm-4:nth-child(1) > a:nth-child(1)')
+# selects the domain based on your location
+books_button = browser.find_element(by=By.CSS_SELECTOR, value='div.col-sm-4:nth-child(1) > a:nth-child(1)')
 books_button.click()
-#selects the domain based on your location
-wait=WebDriverWait(browser,10)
-search_element=wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="searchFieldx"]')))
-search_element.click()
-#wait for the search bar to appear and then click
+
+# creating a waiting object that halts the program after 10 seconds of waiting
+wait = WebDriverWait(browser,10)
+
 
 def extra_options(extension):
-		search_options_button=browser.find_element_by_css_selector('#advSearch-control')
-		#search_options_button=wait.until(EC.element_to_be_clickable((By.XPATH,"html body.index.startForm table tbody tr td.g-page-content div.container div.row div.col-md-12.itemFullText div#searchFormWithLogo form#searchForm div div#advSearch-wrapper div span#advSearch-control")))
-		search_options_button.click()
-		time.sleep(0.3)
-		#extension_button=wait.until(EC.element_to_be_clickable((By.XPATH,"#wrapExt > div:nth-child(1) > input:nth-child(1)")))
-		extension_button=browser.find_element_by_css_selector('#wrapExt > div:nth-child(1) > input:nth-child(1)')
-		extension_button.click()
-		if extension=='pdf':
-
-			extension_pdf=browser.find_element_by_css_selector('div.multiselect-item-wrap:nth-child(9) > span:nth-child(2)')
-			extension_pdf.click()
-
-		elif(extension=='epub'):
-			extension_epub=browser.find_element_by_css_selector('div.multiselect-item-wrap:nth-child(5) > span:nth-child(2)')
-			extension_epub.click()
-		elif(extension=='txt'):
-			extension_txt=browser.find_element_by_css_selector('div.multiselect-item-wrap:nth-child(11) > span:nth-child(2)')
-			extension_text.click()
-		else:
-			return None
-i=0
+    
+    # click on the advanced search button
+    search_options_button = browser.find_element(by=By.CSS_SELECTOR, value='#advSearch-control')
+    
+    search_options_button.click()
+    
+    # wait until the extension button appears and then click on it
+    extension_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#wrapExt > div:nth-child(1) > input:nth-child(1)')))
+    extension_button.click()
+    # select the extension from the list and click on it
+    extension = browser.find_element(by=By.CSS_SELECTOR, value=f'div.multiselect-item-wrap:nth-child({extension_dictionary[extension]})> span:nth-child(2)')
+    extension.click()
+    
+def download_book(book):
+    # type the book name and submit it 
+    search_element.send_keys(book)
+    search_element.submit()
+    # wait for the results 
+    result = wait.until(EC.element_to_be_clickable((By.XPATH,"/html/body/table/tbody/tr[2]/td/div/div/div/div[2]/div[2]/div/table/tbody/tr/td[2]/table/tbody/tr[1]/td/h3/a")))	
+    if result is not None:
+        # click on the result
+        result.click()
+        # find the download button and press it
+        download_button = browser.find_element(by=By.CSS_SELECTOR, value='a.btn-primary')
+        download_button.click()
+             
 for book in book_names:
-	i=i+1
-	if (i>1):
+    # click in the chosen extension
+    extra_options(extension)
+    search_element = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="searchFieldx"]')))
+    search_element.click()
+    # type the book name and submit it
+    download_book(book)
 
-		home=browser.find_element_by_css_selector('#colorBoxes > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)')
-		home.click()
-		extra_options(extension)
-		searchfield=wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="searchFieldx"]')))
-
-		searchfield.click()
-		
-		
-		searchfield.send_keys(book)
-		searchfield.submit()
-		result=wait.until(EC.element_to_be_clickable((By.XPATH,"/html/body/table/tbody/tr[2]/td/div/div/div/div[2]/div[2]/div/table/tbody/tr/td[2]/table/tbody/tr[1]/td/h3/a")))
-		result.click()
-		
-
-
-	else:
-		extra_options(extension)		
-		search_element.send_keys(book)
-		search_element.submit()
-		result=wait.until(EC.element_to_be_clickable((By.XPATH,"/html/body/table/tbody/tr[2]/td/div/div/div/div[2]/div[2]/div/table/tbody/tr/td[2]/table/tbody/tr[1]/td/h3/a")))	
-		if result is not None:
-			result.click()
-			download_button=browser.find_element_by_css_selector('a.btn-primary')
-			download_button.click()
+    # return home for another query
+    home=browser.find_element(by=By.CSS_SELECTOR,value='#colorBoxes > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)')
+    home.click()
+   
